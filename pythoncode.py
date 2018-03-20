bd0e9bf94bb11f5882916dd1fc39040b8931cc17
崔冰 <37291911+verdeeee@users.noreply.github.com>
2018-03-20 19:15:11 +0800

    Add files via upload

---
+++ pythoncode.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 23:39:22 2018
+
+@author: CUIBing
+"""
+
+number = 0 
+while (number < 9): 
+    print ('The number is:', number) 
+    number = number + 1
